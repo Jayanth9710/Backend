8930076fbc1f547a897faf3667c8b6509069bdfb
Jayanth9710 <kumarjayanth55@gmail.com>
2021-09-06 16:26:48 +0530

    Initial commit

---
+++ index.js.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const pinRoute = require("./routes/pins")
+const userRoute = require("./routes/users")
+
+dotenv.config();
+
+const app = express();
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true}).then(()=>{
+    console.log("Mongo DB Connected!")
+}).catch((err)=>console.log(err));
+
+app.use("/api/pins",pinRoute)
+app.use("/api/users",userRoute)
+
+app.listen(8800,() => {
+    console.log("Backend server is running!" )
+});
